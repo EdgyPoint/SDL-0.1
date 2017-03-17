bc9c306a328f39fd1cf4295fbd468712f7d25fb2
Cristian <cristian.palos98@gmail.com>
2017-03-17 17:44:51 +0100

    SDL Game 0.1 Version
    Module Audio in progress

---
+++ ModuleAudio.cpp
@@ -0,0 +1,28 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleAudio.h"
+
+
+
+ModuleAudio::ModuleAudio()
+{
+
+}
+
+
+ModuleAudio::~ModuleAudio()
+{
+
+}
+
+bool ModuleAudio::Init()
+{
+}
+
+// Called before quitting
+bool ModuleAudio::CleanUp()
+{
+
+}
+
+
---
+++ ModuleAudio.h
@@ -0,0 +1,15 @@
+#pragma once
+class ModuleAudio
+
+{
+public:
+
+	ModuleAudio();
+
+	~ModuleAudio();
+
+	bool Init();
+
+	bool CleanUp();
+};
+
